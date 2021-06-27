364a3423da0d2f32098c0bec82bfe24f43a4262c
rg <raska.gergely@gmail.com>
2021-06-27 17:12:20 +0200

    Add nullability check to AddFragment.kt

--- app/src/main/java/hu/rg/cvdatabase/fragment/add/AddFragment.kt
+++ app/src/main/java/hu/rg/cvdatabase/fragment/add/AddFragment.kt
@@ -35,8 +35,7 @@ class AddFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        val rootView =  inflater.inflate(R.layout.fragment_add, container, false)
+        val rootView = inflater.inflate(R.layout.fragment_add, container, false)
 
         mcvViewModel = ViewModelProvider(this).get(CVViewModel::class.java)
 
@@ -91,43 +90,69 @@ class AddFragment : Fragment() {
 
 
 
-        if (inputCheck(name,age)){
-            val cv = CV(name,
+        if (inputCheck(name, age, streetName, streetNumber, cityName, postalCode)) {
+            val cv = CV(
+                name,
                 Integer.parseInt(age),
-                Address(streetName,Integer.parseInt(streetNumber),Integer.parseInt(postalCode),cityName),
-                motivationLetter)
-
-            val job = Job(jobTitle,jobFrom,jobTo,companyName,name)
-
-            val languages : List<Language> = listOf(
-                Language(levelOne,languageOne,name),
-                Language(levelTwo,languageTwo,name),
-                Language(levelThree,languageThree,name))
-
-            val skills : List<Skill> = listOf(
-                Skill(skillOne,name),
-                Skill(skillTwo,name),
-                Skill(skillThree,name)
+                Address(
+                    streetName,
+                    Integer.parseInt(streetNumber),
+                    Integer.parseInt(postalCode),
+                    cityName
+                ),
+                motivationLetter
             )
 
-            val school = School(schoolName,schoolFrom,schoolTo,schoolSubject,name)
+            val job = Job(jobTitle, jobFrom, jobTo, companyName, name)
+
+            val languages: List<Language> = listOf(
+                Language(levelOne, languageOne, name),
+                Language(levelTwo, languageTwo, name),
+                Language(levelThree, languageThree, name)
+            )
+
+            val skills: List<Skill> = listOf(
+                Skill(skillOne, name),
+                Skill(skillTwo, name),
+                Skill(skillThree, name)
+            )
+
+            val school = School(schoolName, schoolFrom, schoolTo, schoolSubject, name)
 
             mcvViewModel.insertCV(cv)
             mcvViewModel.insertJob(job)
             languages.forEach { mcvViewModel.insertLanguage(it) }
             skills.forEach { mcvViewModel.insertSkill(it) }
             mcvViewModel.insertSchool(school)
-            Toast.makeText(requireContext(),"CV succesfully added to database.",Toast.LENGTH_LONG).show()
+            Toast.makeText(requireContext(), "CV succesfully added to database.", Toast.LENGTH_LONG)
+                .show()
             findNavController().navigate(R.id.action_addFragment_to_listFragment)
-        }
-        else {
-            Toast.makeText(this.context,"Please fill name and age fields at least.",Toast.LENGTH_LONG).show()
+        } else {
+            Toast.makeText(
+                this.context,
+                "Please fill name and age fields at least.",
+                Toast.LENGTH_LONG
+            ).show()
         }
 
     }
 
-    private fun inputCheck(name : String, age : String) : Boolean = !(TextUtils.isEmpty(name) && TextUtils.isEmpty(
-        age.toString()
-    ))
+    private fun inputCheck(
+        name: String,
+        age: String,
+        streetName: String,
+        streetNumber: String,
+        cityName: String,
+        postalCode: String
+    ): Boolean =
+        !(
+                TextUtils.isEmpty(name)
+                        && TextUtils.isEmpty(age)
+                        && TextUtils.isEmpty(streetName)
+                        && TextUtils.isEmpty(streetNumber)
+                        && TextUtils.isEmpty(cityName)
+                        && TextUtils.isEmpty(postalCode)
+
+                )
 
 }
